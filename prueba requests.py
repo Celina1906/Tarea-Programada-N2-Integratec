ecbdb2cc3b7dd32d8001f2e0cf1c7ad391781e29
IzLeandro <34462612+IzLeandro@users.noreply.github.com>
2020-11-25 13:24:51 -0600

    Añadido quitarepetidos

--- prueba requests.py
+++ prueba requests.py
@@ -2,28 +2,34 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-lista=[]
-r = requests.get('https://www.tec.ac.cr/carreras')
-contenido = r.text
-#contenido = re.sub('<[^<]+?>', '',contenido)
-#contenido= contenido.replace("\\n","")
-contenido= contenido.rsplit('\n')
-#print(contenido)
-cont=-1
-for i in contenido:
-    #print(i)
-    if '<div class="panel-pane pane-views-panes pane-directory-academic-programs-per-type-panel-pane-2"  >' in i:
-            break
-    if '<h3 class = "group-title">' in i or '<div class="title"><a href=' in i:
-        if '<div class="title"><a href="/programas-academicos/' in i:
-            continue
-        soup = BeautifulSoup(i, 'html.parser')
-        if '<h3 class = "group-title">' in i:
-            cont+=1
-            print(soup.h3.string)
-            lista+=[[soup.h3.string]]
-        else:
-            print("\t",soup.div.string)
-            lista[cont]+=[soup.div.string]
-print(lista)
+def obtenerCarreras():
+    lista=[]
+    r = requests.get('https://www.tec.ac.cr/carreras')
+    contenido = r.text
+    contenido= contenido.rsplit('\n')
+    cont=-1
+    for i in contenido:
+        #print(i)
+        if '<div class="panel-pane pane-views-panes pane-directory-academic-programs-per-type-panel-pane-2"  >' in i:
+                break
+        if '<h3 class = "group-title">' in i or '<div class="title"><a href=' in i:
+            if '<div class="title"><a href="/programas-academicos/' in i:
+                continue
+            soup = BeautifulSoup(i, 'html.parser')
+            if '<h3 class = "group-title">' in i:
+                lista+=[[soup.h3.string]]
+            else:
+                lista[cont]+=[soup.div.string]
+    return lista
 
+def quitaRepetidos(matriz):
+    nuevaMatriz=[]
+    for lista in matriz:
+        listaTemp=[]
+        for i in lista:
+            if not i in listaTemp:
+                listaTemp+=[i]
+        nuevaMatriz+=[listaTemp]
+    return nuevaMatriz
+
+print(quitaRepetidos(obtenerCarreras()))
