010a9f383b64f2f87cd66edd71ca9872c7e3b2f7
Celina1906 <cmadrigalm19@gmail.com>
2020-11-26 20:37:29 -0600

    q

--- Interfaz.py
+++ Interfaz.py
@@ -1,8 +1,8 @@
 #Elaborado por: Leandro Camacho Aguilar y Celina Madrigal Murillo
-#Fecha de Creación: 18/11/2020 6:40pm 
+#Fecha de Creación: 18/11/2020 6:12pm 
 #Fecha de última Modificación: 5/12/2020 9:32pm
 #Versión: 3.9.0
-
+#Importaciones
 from tkinter import*
 
 
@@ -97,7 +97,7 @@ def estudiatesDeCarreraPorSede():
     botonVolver=Button(ventana2,text='Volver al menú principal',width=20,height=2,command= lambda:ventana2.destroy())
     botonVolver.place(x=400,y=200)
 
-#Botones
+#Creación de botones
 boton1=Button(ventanaPrincipal,text='1. Estudiantes por sede',width=18,height=2, command=estudiantesPorSede)
 boton2=Button(ventanaPrincipal,text='2. Estudiantes de carrera por sede',state=DISABLED,width=25,height=2,command=estudiatesDeCarreraPorSede)
 boton3=Button(ventanaPrincipal,text='3. Crear mentores', state=DISABLED,width=14,height=2)
@@ -106,19 +106,18 @@ boton5=Button(ventanaPrincipal,text='5. Actualizar estudiante',state=DISABLED,wi
 boton6=Button(ventanaPrincipal,text='6. Generar reportes',width=18,height=2)
 boton7=Button(ventanaPrincipal,text='7. Crear base de datos en Excel',width=25,height=2)
 boton8=Button(ventanaPrincipal,text='8. Enviar correo',width=13,height=2)
-boton9=Button(ventanaPrincipal,text='9. Eliminar mentor',width=16,height=2)
-boton10=Button(ventanaPrincipal,text='10. Salir',width=10,height=2,command= lambda:ventanaPrincipal.destroy())
-
+boton9=Button(ventanaPrincipal,text='9. Salir',width=10,height=2,command= lambda:ventanaPrincipal.destroy())
+#Colocación de botones
 boton1.place(x=25,y=120)
 boton2.place(x=175,y=120)
 boton3.place(x=385,y=120)
 boton4.place(x=510,y=120)
 boton5.place(x=650,y=120)
-boton6.place(x=25,y=200)
-boton7.place(x=175,y=200)
-boton8.place(x=385,y=200)
-boton9.place(x=510,y=200)
-boton10.place(x=675,y=200)
+boton6.place(x=100,y=200)
+boton7.place(x=260,y=200)
+boton8.place(x=470,y=200)
+boton9.place(x=600,y=200)
+
 
 ventanaPrincipal.mainloop()
 
